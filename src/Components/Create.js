54686c373f7ea15f7d188822e0fbcc635cfa923a
davv1dz <114423472+davv1dz@users.noreply.github.com>
2022-10-10 16:44:57 +0100

    Add files via upload

---
+++ src/Components/Create.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export class Create extends React.Component{
+    render(){
+        return (
+    <div>
+        <h2><center>Hello from my create component</center></h2>
+    </div>
+    );
+  }
+}
